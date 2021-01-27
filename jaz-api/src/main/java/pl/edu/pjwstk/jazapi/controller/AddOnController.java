d962e1e30681379bd725fce029a09c0ad70ed71f
Zevs <piszczatowski.artur@gmail.copm>
2021-01-27 22:30:58 +0100

    AddOnController updated

--- jaz-api/src/main/java/pl/edu/pjwstk/jazapi/controller/AddOnController.java
+++ jaz-api/src/main/java/pl/edu/pjwstk/jazapi/controller/AddOnController.java
@@ -5,11 +5,15 @@ import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.edu.pjwstk.jazapi.dto.AddOnDTO;
+import pl.edu.pjwstk.jazapi.dto.CarDTO;
 import pl.edu.pjwstk.jazapi.model.AddOn;
 import pl.edu.pjwstk.jazapi.service.AddOnService;
 
 import java.util.Collection;
 import java.util.function.Function;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping("/addons")
@@ -21,17 +25,19 @@ public class AddOnController extends CrudController<AddOn, AddOnDTO> {
 
     @Override
     public Function<AddOn, AddOnDTO> transformToDTO() {
-        return null;
+        return AddOnDTO::new;
     }
 
     @Override
     public Function<AddOnDTO, EntityModel<AddOnDTO>> addLinksForItem() {
-        return null;
+        return addon -> EntityModel.of(addon,
+                linkTo(methodOn(AddOnController.class).getById(addon.getId())).withSelfRel(),
+                linkTo(methodOn(AddOnController.class).getAll()).withRel("addons"));
     }
-
     @Override
     public Function<Collection<EntityModel<AddOnDTO>>, CollectionModel<EntityModel<AddOnDTO>>> addLinksForCollection() {
-        return null;
+        return addons -> CollectionModel.of(addons,
+                linkTo(methodOn(AddOnController.class).getAll()).withRel("addons"));
     }
 }
 
